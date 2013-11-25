cfe1576ad2dfb5ee5dd25cbe40bf59143dcb8b45
colinbankier <colinbankier@gmail.com>
2013-11-25 23:38:16 +1000

    Only trigger master branch, with some refactoring.

--- app/go_webhook.rb
+++ app/go_webhook.rb
@@ -6,29 +6,44 @@ require 'dotenv'
 Dotenv.load
 
 post '/notify' do
-  request.body.rewind  # in case someone already read it
-  data = JSON.parse request.body.read
-
-  uri = URI("#{ENV['GO_HOST']}/go/api/pipelines/Supporter/schedule")
-  req = Net::HTTP::Post.new(uri.path)
-  req.body = "materials[#{data['project_name']}]=#{data['commit']['id']}"
-  req.basic_auth ENV['GO_USER'], ENV['GO_PWD']
-  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    puts uri.to_s
-    puts req.body
-    http.request(req)
-  end
-
-  puts res.value
-  puts res.body
-  case res
-  when Net::HTTPSuccess, Net::HTTPRedirection
-    res.body
+  if data['branch_name'] == 'master'
+    uri = URI("#{ENV['GO_HOST']}/go/api/pipelines/Supporter/schedule")
+    req = build_request uri
+    do_request uri, req
   else
-    res.value
+    "Not master branch"
   end
 end
 
-get "/" do
-  request.inspect
+def build_request uri
+    req = Net::HTTP::Post.new(uri.path)
+    req.body = "materials[#{data['project_name']}]=#{data['commit']['id']}"
+    req.basic_auth ENV['GO_USER'], ENV['GO_PWD']
+    req
+end
+
+def data
+  request.body.rewind  # in case someone already read it
+  JSON.parse request.body.read
+end
+
+def do_request uri, req
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      puts uri.to_s
+      puts req.body
+      http.request(req)
+    end
+
+    handle_response res
+end
+
+def handle_response res
+    puts res.value
+    puts res.body
+    case res
+    when Net::HTTPSuccess, Net::HTTPRedirection
+      res.body
+    else
+      res.value
+    end
 end
--- spec/trigger_spec.rb
+++ spec/trigger_spec.rb
@@ -14,7 +14,8 @@ describe 'The GO Webhook App' do
   end
 
   it 'schedules go pipeline' do
-    puts "Go Host: #{ENV['GO_HOST']}"
+    ENV['GO_USER'] = 'user'
+    ENV['GO_PWD'] = 'password'
     stub_request(:any, /user:password@.*/)
     post '/notify', {branch_name: "master", project_name: "supporter", commit: { id: "12345" }}.to_json
 
@@ -22,8 +23,15 @@ describe 'The GO Webhook App' do
     expected_url = "#{ENV['GO_USER']}:#{ENV['GO_PWD']}@#{host}/go/api/pipelines/Supporter/schedule"
     expected_body = "materials[supporter]=12345"
 
-    puts last_response
     expect(last_response).to be_ok
     WebMock.should have_requested(:post, expected_url).with { |req| req.body == expected_body }
+  end
+
+  it 'should not schedule pipeline if branch is not master' do
+    stub_request(:any, /.*/)
+    post '/notify', {branch_name: "my_other_branch", project_name: "supporter", commit: { id: "12345" }}.to_json
+
+    expect(last_response).to be_ok
+    WebMock.should_not have_requested(:get, /.*/)
   end
 end
