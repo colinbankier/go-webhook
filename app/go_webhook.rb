febbd4e589e9938634623ec82acbaf48d7ae71a4
Colin Bankier <colinbankier@gmail.com>
2014-05-07 04:36:10 +0000

    Updated go_webhook to work with the new payment pipeline

--- app/go_webhook.rb
+++ app/go_webhook.rb
@@ -12,7 +12,7 @@ post '/notify' do
   elsif successful_master(data) && data['project_name'] == 'command_centre'
     trigger_pipeline "Heroix_Staging_1", "variables[COMMIT]=#{data['commit']['id']}"
   elsif successful_master(data) && data['project_name'] == 'payments'
-    trigger_pipeline "cobra_prod", "materials[#{data['project_name']}]=#{data['commit']['id']}"
+    trigger_pipeline "payments_production", "materials[#{data['project_name']}]=#{data['commit']['id']}"
   else
     logger.info "Skipping non master branch or failed build - #{data['project_name']}"
     "Not master branch"
